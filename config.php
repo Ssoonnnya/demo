0bf9c88778e83302094ab7aea85c90eddb331754
ssoonnnya <SofiKonors@gmail.com>
2023-04-04 21:37:42 +0300

    Environments and Configuration Flexibility

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return [
+    "database"=>[
+    
+        "host"=>"localhost",
+        "port"=>3306,
+        "dbname"=>"myapp",
+        "charset"=>"uft8mb4"
+    ]
+];
+
+
+
+
+
+
+
+
+?>
