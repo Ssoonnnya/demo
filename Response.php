33480b87cb5b56d47405d66b0975bb3c000bcd9f
ssoonnnya <SofiKonors@gmail.com>
2023-04-06 18:33:16 +0300

    Episode 23

---
+++ Response.php
@@ -0,0 +1,15 @@
+<?php
+
+class Response{
+
+const NOT_FOUND = 404;
+const FORBIDDEN = 403;
+
+}
+
+
+
+
+
+
+?>
--- controllers/notes.php
+++ controllers/notes.php
@@ -7,5 +7,5 @@ $heading = 'My Notes';
 
 $notes = $db->query('select * from notes where user_id = 1')->fetchAll();
 
-require "views/notes.view.php";
+require "view/notes.view.php";
 ?>
