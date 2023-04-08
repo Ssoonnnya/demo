674056c419a4d791c8d08b8b6d0db594148bd60a
ssoonnnya <SofiKonors@gmail.com>
2023-04-08 14:24:20 +0300

    Episode 28 upd

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,23 +6,13 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 $heading = "Create Note";
-$heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
-
-}
     $errors = [];
 
-
-    if(! Validator::string($_POST['body'], 1, 1000)){
-
-        $errors["body"] = "A body of no more than 1,000 characters is required.";
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
-
 
     if(empty($errors)){
 
@@ -33,6 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     }
 
+}
 
 require 'view/note-create.view.php';
 
